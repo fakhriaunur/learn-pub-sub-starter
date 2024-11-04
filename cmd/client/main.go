b079e7d7f5eb4ddf81cb3aef3a9c97ee009eceb1
Fakhri <fakhri.aunur@gmail.com>
2024-11-04 12:19:43 +0700

    Add transient queue implementation

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatalf("couldn't dial the url: %v", err)
+	}
+	defer conn.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("couldn't retrieve the username: %v", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn, routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey, 1,
+	)
+	if err != nil {
+		log.Fatalf("couldn't declare and bind: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	interruptSignal := <-signalChan
+
+	fmt.Printf("\n%s signal received\n", interruptSignal.String())
+	fmt.Println("Peril client is shutting down...")
+
+	// ch, err := conn.Channel()
+	// if err != nil {
+	// 	log.Fatalf("couldn't open the channel: %v", err)
+	// }
+
 }
---
+++ internal/pubsub/queue.go
@@ -0,0 +1,37 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType int,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("coudn't open the channel: %w", err)
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName, simpleQueueType == 0, simpleQueueType == 1,
+		simpleQueueType == 1, false, nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("couldn't declare the queue: %w", err)
+	}
+
+	if err := ch.QueueBind(
+		queueName, key, exchange,
+		false, nil,
+	); err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("couldn't bind the queue: %w", err)
+	}
+
+	return ch, queue, nil
+}
