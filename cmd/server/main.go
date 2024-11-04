0c1982cae077f5f841379a20343bc1b0a541d83a
Fakhri <fakhri.aunur@gmail.com>
2024-11-04 12:58:59 +0700

    Add decoupling logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -24,16 +25,42 @@ func main() {
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("couldn't open the channle: %v", err)
+		log.Fatalf("couldn't open the channel: %v", err)
 	}
 
-	if err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		}); err != nil {
-		log.Fatalf("couldn't publish json: %v", err)
+	gamelogic.PrintServerHelp()
+	for {
+		words := gamelogic.GetInput()
+
+		if len(words) == 0 {
+			continue
+		}
+		if words[0] == "pause" {
+			if err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				}); err != nil {
+				log.Fatalf("couldn't publish json: %v", err)
+			}
+			fmt.Println("Pause message sent!")
+
+		} else if words[0] == "resume" {
+			if err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				}); err != nil {
+				log.Fatalf("couldn't publish json: %v", err)
+			}
+			fmt.Println("Resume message sent!")
+
+		} else if words[0] == "quit" {
+			fmt.Println("Exiting the game...")
+			break
+
+		} else {
+			fmt.Println("couldn't understand the command")
+		}
 	}
-	fmt.Println("Pause message sent!")
 
 	// wait for ctrl + c
 	signalChan := make(chan os.Signal, 1)
